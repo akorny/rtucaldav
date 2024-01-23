9bec2bfc066711998a6b729d28a4b315bb0df66f
Aleksandrs Korņijenko <aleksandrs.kornienko@protonmail.com>
2024-01-23 07:54:33 +0200

    Allow to pass any CalDav url prefix

--- startup.sh
+++ startup.sh
@@ -1,7 +1,10 @@
 export RADICALE_CONFIG=/opt/radicale/radicale-config
-export DJANGO_SUPERUSER_USERNAME=admin
-export DJANGO_SUPERUSER_EMAIL=adminer@example.com
-export DJANGO_SUPERUSER_PASSWORD=admin
+: "${DJANGO_SUPERUSER_USERNAME:=admin}"
+: "${DJANGO_SUPERUSER_EMAIL:=aabbcc@example.com}"
+: "${DJANGO_SUPERUSER_PASSWORD:=admin}"
+: "${HOST:='127.0.0.1'}"
+: "${CSRF_ORIGIN:='http://127.0.0.1'}"
+: "${CALDAV_PUBLIC_URL:='http://127.0.0.1:5232'}"
 
 DJANGO_SECRET_KEY=$(openssl rand -hex 60)
 DJANGO_API_SECRET_KEY=$(openssl rand -hex 60)
@@ -14,12 +17,15 @@ python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install -r requirements.txt
 cd rtucaldav
+
 sed -i "s|STRING_TO_CHANGE_1|$DJANGO_SECRET_KEY|" local_settings.py
 sed -i "s|STRING_TO_CHANGE_2|$CALDAV_PASSWORD|" local_settings.py
+sed -i "s|STRING_TO_CHANGE_5|$DJANGO_API_SECRET_KEY|" local_settings.py
+
 sed -i "s|STRING_TO_CHANGE_3|$HOST|" local_settings.py
 sed -i "s|STRING_TO_CHANGE_4|$CSRF_ORIGIN|" local_settings.py
-sed -i "s|STRING_TO_CHANGE_5|$DJANGO_API_SECRET_KEY|" local_settings.py
-sed -i "s|STRING_TO_CHANGE_6|$CSRF_ORIGIN|" local_settings.py
+sed -i "s|STRING_TO_CHANGE_6|$CALDAV_PUBLIC_URL|" local_settings.py
+
 cd ..
 python3 manage.py migrate
 python3 manage.py collectstatic
